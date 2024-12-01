f766e6b1ff4a5f388905159f772de4df634dcd28
Andrew Stocker <sleepyams@gmail.com>
2024-12-01 12:52:23 -0500

    day1

---
+++ day1.py
@@ -0,0 +1,14 @@
+lines = [line.split() for line in open("day1.txt").read().splitlines()]
+
+list1 = sorted(map(int, [line[0] for line in lines]))
+list2 = sorted(map(int, [line[1] for line in lines]))
+
+
+# Part 1
+print(sum(map(lambda t: abs(t[0] - t[1]), zip(list1, list2))))
+
+
+# Part 2
+counts = { x: list2.count(x) for x in list1 }
+
+print(sum(map(lambda x: x * counts[x], list1)))
